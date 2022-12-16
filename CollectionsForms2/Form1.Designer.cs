82f7098124ff9be714e6478e86cbedbe1c152aa0
CBush88 <christopherbush88@gmail.com>
2022-12-16 13:29:08 -0500

    Design update

--- CollectionsForms2/Form1.Designer.cs
+++ CollectionsForms2/Form1.Designer.cs
@@ -67,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(263, 78);
             this.Controls.Add(this.btn_hashset);
             this.Controls.Add(this.btn_dictionary);
             this.Controls.Add(this.btn_list);
--- CollectionsForms2/Form2.Designer.cs
+++ CollectionsForms2/Form2.Designer.cs
@@ -106,7 +106,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(427, 243);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_reset);
