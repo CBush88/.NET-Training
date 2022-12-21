d97e18d4225efb4821ec245ba81a91ee5eaa6413
CBush88 <christopherbush88@gmail.com>
2022-12-21 17:03:36 -0500

    Generic Collections GUI Practice Problem

---
+++ ProgrammingPractice_GenericCollectionsGUI/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//As per the code implementation using Shape class and other representations of Shape 
+
+//Try to create a generic list (collection) of objects catering all the types of Shape representations 
+
+//e.g. Use System.Collections.Generic.List class
+
+namespace ProgrammingPractice_GenericCollectionsGUI
+{
+    public partial class Form1 : Form
+    {
+        List<Shape> shapes = new List<Shape>();
+        string type;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Base: ";
+            label3.Text = "Height: ";
+            VisibilityOn();
+            type = "triangle";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Width: ";
+            label3.Text = "Length: ";
+            VisibilityOn();
+            type = "rectangle";
+        }
+
+        private void VisibilityOn()
+        {
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            button3.Visible= true;
+        }
+        private void VisibilityOff()
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button3.Visible= false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+
+            switch (type)
+            {
+                case ("triangle"):
+                    shapes.Add(new Triangle(x, y));
+                    break;
+                case ("rectangle"):
+                    shapes.Add(new Rectangle(x, y));
+                    break;
+            }
+            VisibilityOff();
+            textBox1.Clear();
+            textBox2.Clear();
+            label4.Text = string.Empty;
+            foreach(Shape shape in shapes)
+            {
+                label4.Text += shape.GetType().Name + " " + shape.ToString() + "\n";
+            }
+        }
+    }
+}
---
+++ ProgrammingPractice_GenericCollectionsGUI/Shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingPractice_GenericCollectionsGUI
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+        public virtual void Show()
+        {
+            Console.WriteLine("Show method of class Shape");
+        }
+    }
+    public class Triangle: Shape
+    {
+        public double Base;
+        public double Height;
+
+        public override double Area()
+        {
+            return (Base * Height) / 2;
+        }
+
+        public Triangle(double tribase, double height)
+        {
+            Base = tribase;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Base: {Base} Height: {Height} Area: {Area()}";
+        }
+    }
+    public class Rectangle: Shape 
+    {
+        public double Length;
+        public double Width;
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            return $"Length: {Length} Width: {Width} Area: {Area()}";
+        }
+    }
+}
