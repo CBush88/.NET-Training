b7895b14a701f8098eff894b0d3f5fc4210e97e8
CBush88 <christopherbush88@gmail.com>
2022-12-22 17:04:50 -0500

    LINQ examples

---
+++ CsharpTestProject/LINQtoObjectsDemo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTestProject
+{
+    class EmployeData
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Location { get; set; }
+    }
+    internal class LINQtoObjectsDemo
+    {
+        static void Main(string[] args)
+        {
+            //Create object of class and create list of employees
+            List<EmployeData> objEmp = new List<EmployeData>()
+            {
+                new EmployeData{EmpId = 1, EmpName = "Chris", Location = "Work"},
+                new EmployeData{EmpId = 2, EmpName = "Bob", Location = "Home"},
+                new EmployeData{EmpId = 3, EmpName = "Sara", Location = "Canada"},
+                new EmployeData{EmpId = 4, EmpName = "John", Location = "Florida"},
+                new EmployeData{EmpId = 5, EmpName = "Richard", Location = "Georgia"}
+            };
+
+            //var result = from e in objEmp
+            //             where e.Location.Equals("Florida")
+            //             select new {Name = e.EmpName, Location = e.Location};
+
+            Console.WriteLine("Employee data after filter operation");
+
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine("Name: " + item.Name + "\tLocation: " + item.Location);
+            //}
+
+            var result1 = objEmp.Where(e => e.Location.Equals("Florida")).Select(e => new {Name = e.EmpName, Location = e.Location}).ToList();
+            result1.ForEach(e => Console.WriteLine("Name: " + e.Name + "\tLocation: " + e.Location));
+
+            var result2 = objEmp.OrderBy(e => e.Location).ToList();
+            result2.ForEach(e => Console.WriteLine(e.EmpName + "\t" + e.Location));
+
+            var groupings = objEmp.GroupBy(e => e.Location.Length).ToList();
+            foreach(var group in groupings)
+            {
+                foreach(var emp in group)
+                {
+                    Console.Write(emp.EmpName + " Location: " + emp.Location + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            groupings.ForEach(group => {
+                group.ToList()
+                .ForEach(emp => Console.WriteLine(emp.EmpName + " Location: " + emp.Location + "\t"));
+                Console.WriteLine();
+            });
+        }
+    }
+}
---
+++ CsharpTestProject/QuerySyntaxDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTestProject
+{
+    internal class QuerySyntaxDemo
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 4, 1, 3, 9, 8, 7, 6, 2, 0 };
+
+            var filterResult = from num in numbers
+                               where num < 3 || num > 7
+                               select num;
+
+            Console.WriteLine("Filter query result:");
+
+            foreach(var result in filterResult)
+            {
+                Console.Write(result + " ");
+            }
+
+            Console.WriteLine();
+
+            //Order by result
+            Console.WriteLine("Order Result:");
+
+            var orderResult = from num in numbers
+                              where num < 3 || num > 7
+                              orderby num ascending
+                              select num;
+
+            foreach(var result in orderResult)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+
+            //Group by
+
+            string[] groupingQuery = { "cabbage", "broccoli", "beans", "barley", "carrot" };
+
+            IEnumerable<IGrouping<char, string>> foodGroups = from items in groupingQuery
+                                                              group items by items[0];
+            Console.WriteLine("Grouping query result:");
+            foreach(var gResult in foodGroups)
+            {
+                foreach(var item in gResult)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Queries with method syntax
+            // Using lambda
+
+            var largeNumbers = numbers.Where(c => c > 5).ToList();
+            largeNumbers.ForEach(c => Console.Write(c + " "));
+        }
+    }
+}
