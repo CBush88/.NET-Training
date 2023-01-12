b7d88a2a8f657596ad72dd7ecf4452b4edae16c3
CBush88 <christopherbush88@gmail.com>
2023-01-12 16:49:27 -0500

    ASP.NET MVC Code First Web App

---
+++ CodeFirstApp/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApp.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] //allow user input of CourseId
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
