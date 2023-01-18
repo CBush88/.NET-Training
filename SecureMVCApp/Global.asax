5b6a777c523024a725651dbc8b92123ddef8da19
CBush88 <christopherbush88@gmail.com>
2023-01-18 10:57:16 -0500

    ASP.NET Authentication Pre-Built

---
+++ SecureMVCApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecureMVCApp.MvcApplication" Language="C#" %>
---
+++ SecureMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecureMVCApp.Startup))]
+namespace SecureMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
