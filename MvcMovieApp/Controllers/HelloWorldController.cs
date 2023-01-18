6872d01fe39d165024b512de3b642816111b1090
CBush88 <christopherbush88@gmail.com>
2023-01-18 10:56:24 -0500

    ASP.NET Core MVC

---
+++ MvcMovieApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Encodings.Web;
+
+namespace MvcMovieApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome (string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MvcMovieApp/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovieApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to the test page.";
+        }
+        public string Test(string input)
+        {
+            return "This page will print anything you put as the input GET parameter: " + input;
+        }
+    }
+}
