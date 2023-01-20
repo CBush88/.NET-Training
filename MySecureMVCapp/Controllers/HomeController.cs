0d384afebc8cbe0aef14dc77f235434b7b70b6b8
CBush88 <christopherbush88@gmail.com>
2023-01-20 13:03:56 -0500

    ASP.NET Manual Authentication

---
+++ MySecureMVCapp/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MySecureMVCapp.Models;
+
+namespace MySecureMVCapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private readonly Entities _dbContext = new Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = _dbContext.Users
+                    .Any(u => u.Username.ToLower() == user.Username.ToLower()
+                    && u.Password == user.Password);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+            }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Add(registerUser);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
