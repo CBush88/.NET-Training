bb08c9bbbee5df423a5b801dfdd74f2c33e5ac1b
CBush88 <christopherbush88@gmail.com>
2023-02-17 12:04:18 -0500

    Web API Demo

---
+++ WebAPIDemo/Models/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIDemo.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add (Product product);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+}
